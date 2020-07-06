76244439e6eb84b2d429532d64aa8adf4474cb1d
fatima97j <fatimajaffal.1997@gmail.com>
2020-07-06 12:31:35 +0300

    add group anagrams problem

---
+++ groupAnagrams.cpp
@@ -0,0 +1,24 @@
+/*
+  An anagram of a string is another string that contains same characters,
+  only the order of characters can be different.
+  useful link ==> https://www.youtube.com/watch?v=0I6IL3TnIZs&list=PLEJXowNB4kPzM42NGnS_9ok5c3iVIx551&index=3
+*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& s) {
+        unordered_map<string,vector<string>> groups;
+        for(int i=0;i<s.size();i++){
+            string beforeSort = s[i];
+            sort(s[i].begin(),s[i].end());
+            groups[s[i]].push_back(beforeSort);
+        }
+
+        vector<vector<string> > result;
+
+        for(auto itr=groups.begin();itr!=groups.end();++itr)
+            result.push_back(itr->second);
+
+        return result;
+
+    }
+};
